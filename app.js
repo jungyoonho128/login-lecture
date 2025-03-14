90ecee9f79f8d41dba1e00759d371237acec066d
jungyoonho128 <wjddbsgh14@gmail.com>
2025-03-14 10:20:38 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict"
+
+// 모듈
+const express = require("express");
+const app = express();
+
+// 앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+// 라우팅
+const home = require("./routes/home")
+
+app.use("/", home); // use -> 미들웨어
+
+module.exports = app;
